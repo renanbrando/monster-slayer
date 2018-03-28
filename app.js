e151f164af716fa2a2ccf003b732d6c7cc6ea26e
Renan Brando <reebrando@gmail.com>
2018-03-28 12:48:28 -0300

    Added game source.

---
+++ app.js
@@ -0,0 +1,71 @@
+new Vue({
+    el:"#app",
+    data:{
+        inGame: false,
+        playerHealth: 100,
+        monsterHealth: 100,
+        logs: [],
+        turn: 'player'
+    },
+    methods:{
+        atack: function(){
+            this.monsterHealth -=10;
+            this.log("player", "atk", 10);
+            this.turn = 'monster';
+        },
+        specialAtack: function(){
+            this.monsterHealth -=15;
+            this.log("player", "atk", 15);
+            this.turn = 'monster';
+        },
+        heal: function(){
+            this.playerHealth+=15;
+            this.log("player", "heal", 15);
+            this.turn = 'monster';
+        },
+        giveUp: function(){
+            alert("Giving up already? Let's restart the game.")
+            this.inGame = false;
+            this.resetGame();
+        },
+        log: function(type, action, total){
+            let msg = '';
+            if (action == "atk"){
+                msg = `${type} dealt ${total} damage.`;
+            } else if (action == "heal"){
+                msg = `${type} was healed ${total}.`;
+            }
+            this.logs.push({type: type, msg: msg});
+        },
+        monsterTurn: function(){
+            let damage = Math.floor(Math.random() * 15)
+            this.playerHealth -= damage; 
+            this.log("monster", "atk", damage);
+            this.turn = 'player';
+        },
+        resetGame: function(){
+            this.playerHealth = 100;
+            this.monsterHealth = 100;
+            this.logs = [];
+        }
+    },
+    watch:{
+        turn: function(){
+            if (this.turn == 'monster'){
+                this.monsterTurn();
+            }
+        },
+        playerHealth: function(){
+            if (this.playerHealth < 0){
+                alert("You lost");
+                this.resetGame();
+            }
+        },
+        monsterHealth: function(){
+            if (this.monsterHealth < 0){
+                alert("You won");
+                this.resetGame();
+            }
+        }
+    }
+});
